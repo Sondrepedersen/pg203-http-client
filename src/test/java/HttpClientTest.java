ca9fa00c6f9ffe52c87cd7e2d23d97e569e82732
Sondrepedersen <Sondreravndal@hotmail.com>
2021-09-08 12:03:50 +0200

    Add initial test

---
+++ src/test/java/HttpClientTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+
+    @Test
+    void dummytest (){
+        assertEquals(42 , 6*7);
+    }
+
+}
